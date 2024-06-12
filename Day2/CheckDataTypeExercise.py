2aac28152cb27b80b58decff1b9c4f3003679ef0
SamanthaSamosir <meliorasamantha@gmail.com>
2024-06-12 20:36:58 +0700

    update project day 1

---
+++ Day2/CheckDataTypeExercise.py
@@ -0,0 +1,11 @@
+mystery = 734_529.678
+print(type(mystery))
+"""
+Even though the underscore is there to make the large number more readable, 
+because there is a decimal point it is still a Float. 
+If you printed the number in your code, 
+it would be stripped down to 734529.678
+
+In Python, the use of underscores (_) within numeric literals is allowed and is often used 
+to improve readability of large numbers. These underscores do not affect the type of the number.
+"""
