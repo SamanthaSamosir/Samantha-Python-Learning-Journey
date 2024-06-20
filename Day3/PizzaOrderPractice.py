f2eb7d62d5f48ddf805b15d09a370ddd0b13c283
SamanthaSamosir <meliorasamantha@gmail.com>
2024-06-20 13:04:52 +0700

    update project day 3

---
+++ Day3/PizzaOrderPractice.py
@@ -0,0 +1,21 @@
+print("Thank you for choosing Samantha's Pizza Deliveries!")
+size=input("What size of pizza do you want? (S/M/L) ")
+addPepperoni=input("Do you want pepperoni? (Y/N) ")
+extraCheese=input("Do you want an extra cheese? (Y/N) ")
+#psst this is in IDR or Indonesian Rupiah ya :D
+bill=0
+if size=="S":
+    bill+=15000
+elif size=="M":
+    bill+=20000
+else:
+    bill+=29000
+
+if addPepperoni=="Y":
+    if size=="S":
+        bill+=29000
+    else:
+        bill+=35000
+if extraCheese=="Y":
+    bill+=9000
+print(f"Your final bill is Rp{bill}")
