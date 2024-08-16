d5758f3f7ecbe2421e6e6bea83ac7adb5d3059e1
SamanthaSamosir <meliorasamantha@gmail.com>
2024-08-16 23:53:17 +0700

    Update learning Day 2

--- Day2_Understanding Data Types and How to Manipulate Strings/PythonPrimitivesDataTypes.py
+++ Day2_Understanding Data Types and How to Manipulate Strings/PythonPrimitivesDataTypes.py
@@ -1,4 +1,5 @@
-print("Hello"[3])
+print("Hello"[3]) #0 1 2 3
+print("Hello"[-5])#5 4 3 2 1
 """
 The method above called subscripting. 
 A method of pulling out a particular element for a string.
@@ -6,3 +7,6 @@ A method of pulling out a particular element for a string.
 name="Samantha"
 for i in name:
     print(i)
+#backward loop
+for names in name:
+    print(names)
