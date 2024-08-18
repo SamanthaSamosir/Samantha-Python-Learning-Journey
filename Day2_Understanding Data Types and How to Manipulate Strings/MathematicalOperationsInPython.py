65231a24b136397d7fe5754fb65b5430dcb4c806
SamanthaSamosir <meliorasamantha@gmail.com>
2024-08-18 17:58:22 +0700

    Update learning Day 2

---
+++ Day2_Understanding Data Types and How to Manipulate Strings/MathematicalOperationsInPython.py
@@ -0,0 +1,11 @@
+#Remember PEMDAS (Parantheses, Exponents, Multiplication/Division, Addition/Substraction)
+print(3*3+3/3-3) #multiplication,division, addition, substraction
+print("My age: "+str(99))
+print(700+77)
+print(700-77)
+print(700*77)
+print(700/77)
+print(77/7)
+print(77//7)#If you don't want to have .0 behind the result, use this symbol (/) instead.
+print(77**7)#exponent
+print(3*((3+3)/3)-3)
---
+++ Day2_Understanding Data Types and How to Manipulate Strings/NumberManipulationAndFStringsInPython.py
@@ -0,0 +1,25 @@
+"""bmi=62.25/(1.60**2)
+print(bmi)
+print(round(bmi,3))
+print(round(bmi,2))
+print(int(bmi))
+
+weight=61
+weight+=2
+print(weight)
+weight-=1
+print(weight)
+weight*=3
+print(weight)
+weight//=2
+print(weight)
+weight/=3
+print(weight)"""
+
+score=0
+height=1.60
+isWinning=True
+print(f"Your score is {score}. Your height is {height} meters. Jesus is the {isWinning} God.")
+"""
+Use f string so you don't need to convert the data type one by one
+"""
