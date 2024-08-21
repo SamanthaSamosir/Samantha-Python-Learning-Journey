4caa07bfda2a681b9024aad86af56fc72bedb975
SamanthaSamosir <meliorasamantha@gmail.com>
2024-08-21 22:29:42 +0700

    Update learning Day 3

---
+++ Day3_Control Flow and Logical Operators/ControlFlowWithIfElseandConditionalOperators.py
@@ -0,0 +1,6 @@
+print("Welcome to the rollercoaster!")
+height=int(input("What is your height in cm? "))
+if height >120:
+    print("You can ride it. Go ahead!")
+else:
+    print("Sadly, you're not tall enough. Come back when you have grown enough!")
