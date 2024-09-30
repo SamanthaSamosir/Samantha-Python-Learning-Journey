ef908c507d6192e49dd4e0a981325434240baa3d
SamanthaSamosir <meliorasamantha@gmail.com>
2024-09-30 14:35:33 +0700

    Update learning Day 4

--- Day4_RandomisationAndPythonLists/UnderstandingTheOffsetAndAppendingItemsToLists.py
+++ Day4_RandomisationAndPythonLists/UnderstandingTheOffsetAndAppendingItemsToLists.py
@@ -1,4 +1,6 @@
 #We're learning List today hahaha
-Indonesia = ["Jawa Barat", "Jawa Tengah", "Jawa Timur", "Banten", "DKI Jakarta", "DI Yogyakarta", "Aceh", "Sumatera Utara", "Sumatera Barat", "Sumatera Selatan", "Jambi", "Lampung", "Bengkulu", "Bali"]
+Indonesia = ["Jawa Barat", "Jawa Tengah", "Jawa Timur", "Banten", "DKI Jakarta", "DI Yogyakarta", "Aceh",
+             "Sumatera Utara", "Sumatera Barat", "Sumatera Selatan", "Jambi", "Lampung", "Bengkulu", "Bali"]
 print(len(Indonesia))
 print(Indonesia[3])
+print(Indonesia[-2]) #to read from behind
