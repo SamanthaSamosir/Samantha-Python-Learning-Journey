f73a10a2a54b6833a284fcc82b531ac7c39f9ea5
SamanthaSamosir <meliorasamantha@gmail.com>
2024-09-30 15:05:48 +0700

    Update learning Day 4

--- Day4_RandomisationAndPythonLists/UnderstandingTheOffsetAndAppendingItemsToLists.py
+++ Day4_RandomisationAndPythonLists/UnderstandingTheOffsetAndAppendingItemsToLists.py
@@ -7,3 +7,7 @@ print(Indonesia[-2]) #to read from behind
 print(Indonesia)
 Indonesia[1] = "Jateng" #to change value/item Jawa Tengah to Jateng
 print(Indonesia)
+Indonesia.append("Kalimantan Utara") #to add one value/item inside the List.
+print(Indonesia)
+Indonesia.extend(["Kalimantan Barat", "NTT"]) #to add more than one value/item to the List.
+print(Indonesia)
