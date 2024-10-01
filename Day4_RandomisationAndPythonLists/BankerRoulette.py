ec047e9979d25cdaea4c6918fd3f076427440d48
SamanthaSamosir <meliorasamantha@gmail.com>
2024-10-01 10:58:14 +0700

    Update learning Day 4

--- Day4_RandomisationAndPythonLists/BankerRoulette.py
+++
@@ -1,4 +0,0 @@
-#Who will pay the bill?
-#Pssttt... Men should always pay the bills.
-
-name=namesString.split(", ")
---
+++ Day4_RandomisationAndPythonLists/WhoWillPayTheBill.py
@@ -0,0 +1,7 @@
+#Who will pay the bill?
+import random
+#use choice for sequence
+friends = ["Samantha", "Lisa", "Yola", "Ridho", "Andi", "Rei"]
+ran = random.choice(friends)
+print(ran)
+print(random.choice(friends))
