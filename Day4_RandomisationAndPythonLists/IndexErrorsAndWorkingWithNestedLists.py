cf8465d201607bdb63df76c714598a3085ab08af
SamanthaSamosir <meliorasamantha@gmail.com>
2024-10-01 15:05:00 +0700

    Update learning Day 4

--- Day4_RandomisationAndPythonLists/IndexErrorsAndWorkingWithNestedLists.py
+++ Day4_RandomisationAndPythonLists/IndexErrorsAndWorkingWithNestedLists.py
@@ -2,5 +2,9 @@ veggies = ["spinach","kale","collard greens","bell peppers",
            "hot peppers","celery","tomatoes","cucumbers","lettuce","potatoes","green beans","peas","zucchini","summer squash"]
 fruits = ["rawberries", "apples", "grapes", "pears", "cherries", "peaches", "nectarines", "blueberries", "raspberries", "plums", "citrus fruits", "apricots", "kiwis"]
 #Let's create nested List
-dirtyDozen=[veggies, fruits]
+dirtyDozen=[veggies, fruits] #[0][1]
 print(dirtyDozen)
+print(dirtyDozen[0])
+print(dirtyDozen[1])
+print(dirtyDozen[1][2]) #[list][items inside the list]
+print(dirtyDozen[0][3])
