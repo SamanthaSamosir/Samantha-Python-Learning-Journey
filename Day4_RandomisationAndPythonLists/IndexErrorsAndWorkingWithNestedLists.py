e79a4be11c08ab9171adda8494f772b4114e489f
SamanthaSamosir <meliorasamantha@gmail.com>
2024-10-01 14:42:32 +0700

    Update learning Day 4

---
+++ Day4_RandomisationAndPythonLists/IndexErrorsAndWorkingWithNestedLists.py
@@ -0,0 +1,6 @@
+veggies = ["spinach","kale","collard greens","bell peppers",
+           "hot peppers","celery","tomatoes","cucumbers","lettuce","potatoes","green beans","peas","zucchini","summer squash"]
+fruits = ["rawberries", "apples", "grapes", "pears", "cherries", "peaches", "nectarines", "blueberries", "raspberries", "plums", "citrus fruits", "apricots", "kiwis"]
+#Let's create nested List
+dirtyDozen=[veggies, fruits]
+print(dirtyDozen)
--- Day4_RandomisationAndPythonLists/WhoWillPayTheBill.py
+++ Day4_RandomisationAndPythonLists/WhoWillPayTheBill.py
@@ -2,6 +2,11 @@
 import random
 #use choice for sequence
 friends = ["Samantha", "Lisa", "Yola", "Ridho", "Andi", "Rei"]
+#option1
 ran = random.choice(friends)
 print(ran)
 print(random.choice(friends))
+
+#option2
+random_index = random.randint(0,len(friends))
+print(friends[random_index])
